27061274aec5330a134d362aa26adbd020359a7a
Emily <emilyharnish49@gmail.com>
2021-05-18 17:20:30 -0400

    added footer to home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import NavBar from './HomePage/NavBar/NavBar.js';
 import HomePageApp from './HomePage/Feed/Feed.js'; 
+import Footer from './HomePage/Footer/Footer.js';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <NavBar/>
       <HomePageApp/>
+      <Footer/>
     </div>
   );
 }
