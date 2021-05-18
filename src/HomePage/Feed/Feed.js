1ea4033afbd4da2855740ea15f5193a9adc37393
Emily <emilyharnish49@gmail.com>
2021-05-18 09:27:30 -0400

    fixed branch confusion

--- src/HomePage/Feed/Feed.js
+++ src/HomePage/Feed/Feed.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'; 
 import { Form } from 'react-bootstrap';
-import React, { Component } from 'react';
 
 import Image from 'react-bootstrap/Image';
 import Container from 'react-bootstrap/Container';
@@ -29,6 +28,36 @@ class HomePageBanner extends Component {
         return (
             <div> 
                 <Image src={Banner} alt="Banner" fluid></Image>
+            </div>
+        );
+    }
+}
+
+class ImagesDisplay extends Component {
+    render() {
+        return (
+            <div>
+                <Container fluid="lg">
+                    <Row>
+                        <Col>
+                            <a href="/knowJesus">
+                                <Image src="https://i.picsum.photos/id/788/200/300.jpg?hmac=86XnLHCHcI7HWgr9Y662VsXxUxs7H70DjGHc_6iaIw4" alt="Know Jesus" className="LinkPics" fluid></Image>
+                            </a>
+                        </Col>
+                        <br/>
+                        <Col>
+                            <a href="/events">
+                                <Image src="https://i.picsum.photos/id/521/200/300.jpg?hmac=_MGlU-tHw5IBlsNL7YvJ9lTMo4Ge605GWQwuKGxWIWU" alt="Events" className="LinkPics" fluid></Image>
+                            </a>
+                        </Col>
+                        <br/>
+                        <Col>
+                            <a href="/getInvolved">
+                                <Image src="https://i.picsum.photos/id/756/200/300.jpg?hmac=kojqQY60yVD4KaSEFOEw62LRuwfiOR2f-6ZdnEgKhxM" alt="Get Involved" className="LinkPics" fluid></Image>
+                            </a>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         );
     }
@@ -65,28 +94,6 @@ class SignUpForm extends Component {
 
                         <Col>
                             <Image src="https://i.picsum.photos/id/19/200/300.jpg?hmac=znGSIxHtiP0JiLTKW6bT7HlcfagMutcHfeZyNkglQFM" alt="Signup Form"></Image>
-class ImagesDisplay extends Component {
-    render() {
-        return (
-            <div>
-                <Container fluid="lg">
-                    <Row>
-                        <Col>
-                            <a href="/knowJesus">
-                                <Image src="https://i.picsum.photos/id/788/200/300.jpg?hmac=86XnLHCHcI7HWgr9Y662VsXxUxs7H70DjGHc_6iaIw4" alt="Know Jesus" className="LinkPics" fluid></Image>
-                            </a>
-                        </Col>
-                        <br/>
-                        <Col>
-                            <a href="/events">
-                                <Image src="https://i.picsum.photos/id/521/200/300.jpg?hmac=_MGlU-tHw5IBlsNL7YvJ9lTMo4Ge605GWQwuKGxWIWU" alt="Events" className="LinkPics" fluid></Image>
-                            </a>
-                        </Col>
-                        <br/>
-                        <Col>
-                            <a href="/getInvolved">
-                                <Image src="https://i.picsum.photos/id/756/200/300.jpg?hmac=kojqQY60yVD4KaSEFOEw62LRuwfiOR2f-6ZdnEgKhxM" alt="Get Involved" className="LinkPics" fluid></Image>
-                            </a>
                         </Col>
                     </Row>
                 </Container>
@@ -95,5 +102,6 @@ class ImagesDisplay extends Component {
     }
 }
 
-export default Feed; 
+
+
 export default HomePageApp; 
