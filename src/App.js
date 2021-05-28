839078ae4cf6f1e63c87a1b59ea7f752f4569f43
Michael Klennert <michael.klennert@gmail.com>
2021-05-27 20:40:24 -0700

    Cleaned Up Navbar and Login Admin Routing

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import Footer from './HomePage/Footer/Footer.js';
 import LoginApp from './Login/LoginApp';
 import VideoFeed from './VideosPage/Videos'
 import VideoApp from './VideosPage/Videos.js';
-import AdminVideoApp from './VideosPage/VideosTest.js'; 
+import AdminVideoApp from './VideosPage/VideosTest.js';
 import Events from './EventsPage/Events';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -25,42 +25,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-
-      <Router>
-        <Navbar bg="light" expand="lg" className="HomePageNavBar" fixed="sticky">
-          <Navbar.Brand href="/">CCOTH</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
-            <Nav>
-              <Link t0="/">HOME</Link>
-              <Link to="/events">EVENTS</Link>
-              <Link to="/videos">TEACHINGS</Link>
-              <Link to="/login">LOGIN</Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-
-        <Switch>
-          <Route path="/events">
-            <Events />
-          </Route>
-
-          <Route path="/videos">
-            <VideoFeed />
-          </Route>
-
-          <Route path="/login">
-            <LoginApp />
-          </Route>
-
-          <Route path="/">
-            <HomePageApp />
-          </Route>
-        </Switch>
-      </Router>
-
-      <Footer />
-
+      <NavBar />
     </div>
   );
 }
