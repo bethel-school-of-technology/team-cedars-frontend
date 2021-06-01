421c66846a553270db8cfe213debbc04095b0a25
Emily <emilyharnish49@gmail.com>
2021-06-01 09:53:18 -0400

    still working on connecting front end videos page to backend

--- src/VideosPage/VideosTest.js
+++ src/VideosPage/VideosTest.js
@@ -4,23 +4,28 @@ import { Form, Button, Container } from 'react-bootstrap';
 //https://www.youtube.com/embed/5NEe8OkXVzE
 
 class AdminVideoApp extends Component {
-    constructor() {
-        super(); 
+    constructor(props) {
+        super(props); 
         this.state = {
             title: '',
             info: '',
             url: ''
         }
-        this.submitForm =  this.submitForm.bind(this);
     }
 
-    submitForm = () => {
-        this.setState(state => ({
-            title: document.getElementById('titleInput').value,
-            info: document.getElementById('infoInput').value,
-            url: document.getElementById('urlInput').value
-        }));
+    onGetValues = () => {
+        fetch("http://localhost:8080/api/video", {
+            headers: { 'Authorization': this.state.token },
+            method: "POST",
+            body: JSON.stringify({ title: this.state.title, info: this.state.info, url: this.state.url })
+        })
+            .then(res => res.json())
+            .then(json => this.setState({ ...this.state, values: json }));
     }
+
+    onTitleChange = (e) => this.setState({ ...this.state, title: e.target.value });
+    onInfoChange = (e) => this.setState({ ...this.state, info: e.target.value });
+    onUrlChange = (e) => this.setState({ ...this.state, url: e.target.value });
 
     render() {
         return (
@@ -32,20 +37,20 @@ class AdminVideoApp extends Component {
                 <Form>
                     <Form.Group controlId="formBasicText">
                         <Form.Label>Title of Video</Form.Label>
-                        <Form.Control id="titleInput" type="text" placeholder="Please enter the title of the video" />
+                        <Form.Control id="titleInput" type="text" placeholder="Please enter the title of the video" onChange={this.onTitleChange}/>
                     </Form.Group>
 
                     <Form.Group controlId="formBasicText">
                         <Form.Label>Short Description</Form.Label>
-                        <Form.Control id="infoInput" type="text" placeholder="Please enter a short description" />
+                        <Form.Control id="infoInput" type="text" placeholder="Please enter a short description" onChange={this.onInfoChange}/>
                     </Form.Group>
 
                     <Form.Group controlId="formBasicUrl">
                         <Form.Label>Url of Video</Form.Label>
-                        <Form.Control id="urlInput" type="url" placeholder="Please enter the video url" />
+                        <Form.Control id="urlInput" type="url" placeholder="Please enter the video url" onChange={this.onUrlChange}/>
                     </Form.Group>
 
-                    <Button variant="primary" type="submit" onClick={this.submitForm}>Submit</Button>
+                    <Button variant="primary" type="submit" onClick={this.onGetValues}>Submit</Button>
                 </Form>
                 </Container>
 
@@ -63,5 +68,3 @@ class AdminVideoApp extends Component {
 
 
 export default AdminVideoApp
-
-
