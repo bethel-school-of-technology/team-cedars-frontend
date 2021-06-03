d413ba4827a008284207be40e15b07d250bb18ff
Michael Klennert <michael.klennert@gmail.com>
2021-06-03 15:21:05 -0700

    Video Feed is Working!!!

--- src/VideosPage/VideoPageArray.js
+++ src/VideosPage/VideoPageArray.js
@@ -5,7 +5,8 @@ import './Videos.css';
 
 const VideoList = () => {
 
-    const [videos, setVideos] = useState();
+    const [videos, setVideos] = useState([]);
+
 
 
     useEffect(() => {
@@ -15,7 +16,39 @@ const VideoList = () => {
         });
     }, []);
 
+    console.log(videos);
+
+
+
+
+    return (
+        <ul>
+            {videos.map(video => (
+                <div key={video.id}>
+                    <br />
+                    <br />
+                    <h1>{video.title}</h1>
+                    <h4>{video.info}</h4>
+                    <iframe
+                        key={video.id}
+                        src={video.url}
+                        width="640"
+                        height="360"
+                        frameborder="0"
+                        title="defaultVideo"
+                        allow="autoplay; fullscreen; picture-in-picture"
+                        allowfullscreen
+                    ></iframe>
+                    <br />
+                    <br />
+                </div>
+            ))}
+        </ul>
+
+
+    );
 }
+
 
 
 
