2c11bfdf70465a3d6ea78290bf9e974d0559923f
Emily <emilyharnish49@gmail.com>
2021-06-04 11:01:23 -0400

    deleted some unused files and cleaned up my code

--- src/App.js
+++ src/App.js
@@ -4,24 +4,16 @@ import {
   Switch,
   Route,
   Link,
-  useRouteMatch,
-  useParams
 } from "react-router-dom";
 import './App.css';
 import './HomePage/NavBar/NavBar.css';
-import NavBar from './HomePage/NavBar/NavBar.js';
 import { Navbar, Nav } from 'react-bootstrap';
 
 import HomePageApp from './HomePage/Feed/Feed.js';
 import Footer from './HomePage/Footer/Footer.js';
 import LoginApp from './Login/LoginApp';
-import VideoFeed from './VideosPage/Videos'
-import VideoApp from './VideosPage/Videos.js';
-// import AdminVideoApp from './VideosPage/VideosTest.js';
 import Events from './EventsPage/Events';
-
-//Added on 2.0 process of Videos Page
-import AdminVideoApp from './VideosPage/AdminVideoApp'; 
+import VideoApp from './VideosPage/Videos'; 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -49,7 +41,7 @@ function App() {
           </Route>
 
           <Route path="/videos">
-            <VideoFeed />
+            <VideoApp />
           </Route>
 
           <Route path="/login">
@@ -63,7 +55,6 @@ function App() {
       </Router>
 
       <Footer />
-      <AdminVideoApp/>
     </div>
   );
 }
